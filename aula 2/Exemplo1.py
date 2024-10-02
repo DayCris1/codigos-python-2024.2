881007bc0bf8322087645a52fa9719828680de33
DayCris1 <dayene_crist@hotmail.com>
2024-10-02 19:32:20 -0300

    exemplo 1 atualização
    melhorei o mapa do tesouro

---
+++ aula 2/Exemplo1.py
@@ -0,0 +1,5 @@
+#Esses serão os passos do pirata
+print("Andar para direita")
+print("Andar para esquerda")
+print("Andar para baixo")
+print("procurar um X e cavar")
--- aula 2/exemplo11.py
+++
@@ -1,4 +0,0 @@
-#Esses serão os passos do pirata
-print("Andar para direita")
-print("Andar para esquerda")
-print("Andar para baixo")
