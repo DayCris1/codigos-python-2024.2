99267d4457a4d6565e9b0c606dd09a9cc2a95c4d
DayCris1 <dayene_crist@hotmail.com>
2024-10-02 21:36:01 -0300

    exercicio 1 ipva
    calculo de ipva

---
+++ aula 3/exercicio 1.py
@@ -0,0 +1,10 @@
+#Vamos calcular o IPVA do Carro
+valorcarro=float(input("coloque o valor do carro"))
+IPVA=valorcarro * 4/100
+print(f"o valor que você terá que pagar de imposto do IPVA é:{IPVA}")
+
+
+
+
+
+
