28487460b73eeef6acef624e4042928452935fb4
DayCris1 <dayene_crist@hotmail.com>
2024-10-03 23:14:09 -0300

    Add files via upload

---
+++ trabalho de casa/exemplo2.py
@@ -0,0 +1,7 @@
+#Vamos calcular o tamanho do sapato do cachorro
+coleira=float(input("coloque o tamanho da coleira:"))
+sapato=(coleira*2)
+print(f"o tamanho do sapato é: {sapato}")
+#Vamos calcular o tamanho da fucinheira
+fucinheira=(sapato*3)
+print(f"o tamanho da fucinheira é: {fucinheira}")
---
+++ trabalho de casa/exercício 1.py
@@ -0,0 +1,8 @@
+#COMPRA DO ARO DA BICICLETA
+aro=float(input("coloque o tamanho do aro:"))
+guidão=(aro/2)
+print(f"o tamanho do guidão é: {guidão}")
+manete=(aro/4)
+print(f"o tamanho da manete é: {manete}")
+quadro=(aro)
+print(f"o tamanho do quadro é: {quadro}")
