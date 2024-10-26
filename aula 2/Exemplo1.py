64234c485a25e0de9db87bf98ee7208cedc4ccd8
DayCris1 <dayene_crist@hotmail.com>
2024-10-25 21:53:03 -0300

    exemplo1
    aula

--- aula 2/Exemplo1.py
+++ aula 2/Exemplo1.py
@@ -1,5 +1,7 @@
 #Esses serão os passos do pirata
+print("Este é o caminho para o mapa do tesouro")
 print("Andar para direita")
 print("Andar para esquerda")
 print("Andar para baixo")
-print("procurar um X e cavar")
+print("procurar o X e cavar")
+print("procurar o X e cavar")
